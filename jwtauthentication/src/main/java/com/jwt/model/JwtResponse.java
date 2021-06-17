b350b0b3736e31864b370bc1db9976c086a64de2
Jatin Gulati <jatingulati8991@gmail.com>
2021-06-17 15:34:29 +0530

    First Commit Uploading all files

---
+++ jwtauthentication/src/main/java/com/jwt/model/JwtResponse.java
@@ -0,0 +1,31 @@
+package com.jwt.model;
+
+public class JwtResponse {
+	
+	public JwtResponse() {
+	
+	}
+
+	String token;
+	
+	
+
+	
+
+	public JwtResponse(String token) {
+		super();
+		this.token = token;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+	
+
+}
